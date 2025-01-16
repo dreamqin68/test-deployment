d72dfd30c1057b9ef8201e5214ad408a3199795d
dreamqin68 <huanwu768@gmail.com>
2025-01-16 06:40:34 -0800

    first commit

---
+++ minimal-backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 8747;
+
+app.use(
+  cors({
+    origin: "https://dreamqin68.github.io",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+// New signup endpoint
+app.post("/api/auth/signup", (req, res) => {
+  // Extract user details from req.body
+  const { username, password } = req.body;
+  console.log("Signup request body:", req.body);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
